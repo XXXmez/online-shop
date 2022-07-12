db3fb4e74e3235842b52f5ba02b082f94dbc729a
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-07-12 07:40:17 +0300

    feat: started making a sorting menu, made sorting by company and sorting by availability

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,8 @@
+
 import "../style/style.scss";
+
+import noUiSlider from "./nouislider.min.js"
+
 import img0 from "../img/sneakers1.jpg"
 import img1 from "../img/sneakers2.jpg"
 import img2 from "../img/sneakers3.jpg"
@@ -6,3 +10,25 @@ import img3 from "../img/sneakers4.jpg"
 import img4 from "../img/sneakers5.jpg"
 import img5 from "../img/sneakers6.jpg"
 import img6 from "../img/sneakers7.jpg"
+
+
+const slider = document.querySelector('.sorts__amount__slider'),
+    sortsAmountMim = document.querySelector('.sorts__amount__mim'),
+    sortsAmountMax = document.querySelector('.sorts__amount__max'),
+    minMax = [sortsAmountMim, sortsAmountMax];
+
+
+if (slider) {
+    noUiSlider.create(slider, {
+        start: [0, 100],
+        connect: true,
+        step: 1,
+        range: {
+            'min': 0,
+            'max': 100
+        }
+    });
+    slider.noUiSlider.on('update', (values, handle) => {
+        minMax[handle].textContent = Math.round(values[handle])
+    })
+}
